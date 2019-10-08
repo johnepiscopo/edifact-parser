21de1efb1ecf00e1bad5965ef2ea2d853cd5e3d5
johnepiscopo <j.episcopo@sky.com>
2019-10-08 10:38:43 +0100

    Added comments

--- edisegments/segmenter.go
+++ edisegments/segmenter.go
@@ -1,10 +1,13 @@
 package edisegments
 
+//Segmenter is the interface defining the contract
+//for each of the EDI Segments
 type Segmenter interface {
 	GetElements() []string
 	GetSeparator(element string) string
 }
 
+//Segments is the map that maps the Segment identifier to the EDI Segment model
 var Segments = map[string]Segmenter{
 	"UNH": NewUNHModel(),
 	"BGM": NewBGMModel(),
